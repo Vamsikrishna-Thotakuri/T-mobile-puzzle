77d4becf26d05621bc0e5a8d1cdfeeb756e18dcc
vthotak1 <vamsikrishna.thotakuri1@t-mobile.com>
2020-05-08 14:24:59 -0700

    Removed apikey from environment.prod file

--- apps/stocks/src/environments/environment.prod.ts
+++ apps/stocks/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: true,
-  apiKey: 'sk_2ee5c125566a435ca74247bbc9bc3788',
+  apiKey: '',
   apiURL: 'https://sandbox.iexapis.com'
 };
